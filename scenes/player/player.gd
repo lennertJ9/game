b8110792891261dc8e172ff5ccfa307ddd234c2f
lennertJ9 <142997753+lennertJ9@users.noreply.github.com>
2024-01-30 10:47:34 +0100

    psuh

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,21 +3,19 @@ extends CharacterBody2D
 
 
 @onready var animation_player1 = $AnimationPlayer
-@onready var animation_player_2 = $AnimationPlayer2
+@onready var animation_player2 = $AnimationPlayer2
 
 @onready var visuals = $Visuals
 @onready var run_dust = $Visuals/RunDust
 @onready var hand_left = $Visuals/HandLeft
-@onready var weapon = $Visuals/HandLeft/Weapon
+@onready var weapon = $Visuals/Weapon
 
 # walking system
 @export var speed = 40
-var current_speed = 60
+var current_speed = 70
 var movement_direction: Vector2
 var target = position
-var is_running
-var is_moving_right: bool
-var is_moving_left: bool
+
 
 
 func _ready():
@@ -26,13 +24,11 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("right_click"):
-		is_running = true
 
 		target = get_global_mouse_position()
 		movement_direction = global_position.direction_to(target)
-		print(movement_direction)
+		
 		if movement_direction.x > 0.6 or movement_direction.x < -0.6:
-			print("side")
 			animation_player1.play("RUN_SIDE")
 			if movement_direction.x > 0:
 				$Visuals.scale.x = 1
@@ -41,49 +37,36 @@ func _input(event):
 				
 		else:
 			print("down")
-			animation_player1.play("RUN_DOWN")
+			if movement_direction.y > 0:
+				animation_player1.play("RUN_DOWN")
+			else:
+				animation_player1.play("RUN_UP")
 		
 
+	if event.is_action_pressed("a"):
 		
+		var animation: Animation = animation_player2.get_animation("IDLE_FRONT")
+		animation.track_set_key_value(0,0,Vector2(-10,0))
 		
-
-
-	if event.is_action_pressed("do_something"):
+		print(animation.track_get_key_value(0,0))
+		
 		
 		#var tween = create_tween()
 		#tween.tween_property(hand_left,"rotation_degrees", + 15, .25).set_trans(Tween.TRANS_BACK)
 		#tween.tween_property(hand_left,"rotation_degrees", -10, .15)
-		var shoot_direction = (get_global_mouse_position() - $Visuals/Marker2D.global_position).normalized()
-		print(shoot_direction)
-		$BoltManager.shoot(shoot_direction,$Visuals/Marker2D.global_position)
-		$AnimationPlayer2.play("ATTACK_LEFT_FRONT")
-		if shoot_direction.x > 0:
-			$Visuals.scale.x = 1
-		else:
-			$Visuals.scale.x = -1
+		#var shoot_direction = (get_global_mouse_position() - $Visuals/Marker2D.global_position).normalized()
+		#print(shoot_direction)
+		#$BoltManager.shoot(shoot_direction,$Visuals/Marker2D.global_position)
+		#$AnimationPlayer2.play("ATTACK_DOWN")
+		#if shoot_direction.x > 0:
+			#$Visuals.scale.x = 1
+		#else:
+			#$Visuals.scale.x = -1
 
 func _process(delta):
 	var direction_to_mouse = (get_global_mouse_position() - global_position).normalized()
 	var target_position = global_position + Vector2(0,-4) + direction_to_mouse * 7
-	
-# weapon rotation -----------------------------------------------------------------------
-	#if direction_to_mouse.x < 0:
-		#$Visuals.scale.x = -1
-		#hand_left.rotation_degrees = rad_to_deg(direction_to_mouse.dot(Vector2.DOWN)) -55
-		
-	#else: 
-		#$Visuals.scale.x = 1
-		#hand_left.rotation_degrees = rad_to_deg(direction_to_mouse.dot(Vector2.DOWN)) -55
-		
-	#hand_left.global_position = target_position
-	
-# reverse walking logic ---------------------------------------------------------------------------------------------------------
-	#if direction_to_mouse.x < 0 and is_moving_right and is_running or direction_to_mouse.x > 0 and is_moving_left and is_running:
-		#animation_player.play("RUN_REVERSE")
-		#print("revers")
-		
-	
-	
+
 
 func _physics_process(_delta):
 	velocity = movement_direction * current_speed
@@ -92,16 +75,14 @@ func _physics_process(_delta):
 		move_and_slide()
 
 	else:
-		is_running = false
-		animation_player1.play("IDLE_DOWN")
+		if animation_player1.current_animation == "RUN_SIDE":
+			animation_player1.play("IDLE_SIDE")
+		if animation_player1.current_animation == "RUN_UP":
+			animation_player1.play("IDLE_UP")
+		if animation_player1.current_animation == "RUN_DOWN":
+			animation_player1.play("IDLE_DOWN")
+		
 
-
-func emit_dust_particles():
-	var particle = preload("res://scenes/player/visuals/RunDust.tscn").instantiate()
-	var world = get_tree().get_first_node_in_group("world")
-	particle.global_position = global_position
-	particle.play()
-	world.add_child(particle)
 	
 func hide_weapon():
 	$AnimationPlayer2.play('HIDE_WEAPON')
